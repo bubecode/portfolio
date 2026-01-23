a0c5b9eee0080d6074543ff0d8feb0d79b0bfd43
bubecode <bube.code@gmail.com>
2026-01-23 07:35:11 +0300

    Security: Ignore database.php and add sample_database.php

---
+++ application/config/sample_database.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+| -------------------------------------------------------------------
+| DATABASE CONNECTIVITY SETTINGS (SAMPLE)
+| -------------------------------------------------------------------
+| Rename this file to database.php and update the settings below.
+*/
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'your_username',
+	'password' => 'your_password',
+	'database' => 'your_database',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
